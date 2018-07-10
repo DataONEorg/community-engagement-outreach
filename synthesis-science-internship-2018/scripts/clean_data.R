ede80fe2c30330530fe57377c12baeadba4841b6
rpecchia <rpecchia@gmail.com>
2018-07-10 09:02:46 -0700

    moved continent script to "scripts" folder added in code to take raw_data from google sheets

--- synthesis-science-internship-2018/scripts/clean_data.R
+++ synthesis-science-internship-2018/scripts/clean_data.R
@@ -2,8 +2,16 @@
 ##  I haven't done much cleaning yet, but when we need to do any cleaning to the entire dataset,
 ##  do it here.
 
+# Load Libraries
 library(tidyverse)
+library(googlesheets)
 
+# Read data from computer
 raw_data <- read.csv("data_input/dataone_SR_raw_data.csv", head = TRUE)
-
 head(raw_data)
+
+# Read data from google sheets
+(my_sheets <- gs_ls()) #get list of sheets you can access, will possible prompt you with a google login and tidyverse permission page
+my_sheets #just to see the list of sheets you can access
+gs1 = gs_title('database_for_systematic_review', verbose = TRUE) #get spreadsheet of interest
+raw_data = gs_read(gs1,ws = "Combined80papers") #get worksheet of interest from spreadsheet
